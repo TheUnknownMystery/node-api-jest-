485463fd84991f9370d3d1c198cebee6bb7ccff7
sauhardo2020 <sauhardo2007@gmail.com>
2022-02-10 23:15:17 +0530

    Added Post With Testing

--- src/router/MovieRouter.js
+++ src/router/MovieRouter.js
@@ -6,20 +6,21 @@ router.get('/', (request, response) => {
   return response.send(movies);
 });
 
-router.get('/movie/:id', (request, response) => {
-  const { id } = request.params;
+router.get('/movie/:id', ({ params: { id } }, response) => {
   const targeted_movie = movies.filter((movie) => movie.id == id);
   response.send(targeted_movie);
 });
 
-router.post('/movie', (request, response) => {
-  const { name, author } = request.body;
+router.post('/movie', ({ body }, response) => {
+  const { name, author } = body;
 
   response.send([
     {
+      id: movies.length + 1,
       name,
       author,
     },
   ]);
 });
+
 module.exports = router;
--- src/tests/MovieRouter.test.js
+++ src/tests/MovieRouter.test.js
@@ -2,14 +2,29 @@ const request = require('supertest');
 const app = require('../app');
 const movies = require('../data/data');
 
-test('should give us the move data', async function () {
+test('should give us the move data', async () => {
   const response = await request(app).get('/');
   expect(response._body).toEqual(movies);
 });
 
-test('should give movie by id', async function () {
+test('should give movie by id', async () => {
   const response = await request(app).get('/movie/2');
-  expect(JSON.parse(response.text)).toEqual([
-    { id: 2, name: 'movie 2', author: 'movie 2 author' },
+
+  expect(JSON.parse(response.text)).toEqual([movies[1]]);
+});
+
+test('should give us an id and send us the posted movie obj', async () => {
+  const example = {
+    name: 'movie 888',
+    author: 'new author',
+  };
+
+  const response = await request(app).post('/movie').send(example);
+
+  expect(response._body).toEqual([
+    {
+      id: movies.length + 1,
+      ...example,
+    },
   ]);
 });
